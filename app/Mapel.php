1ac361293807a33d1607bd033bc2a4061764b139
hananhanafi76 <hananhanafi76@gmail.com>
2019-12-12 22:55:42 +0700

    Add Mapel-siswa

---
+++ app/Mapel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Mapel extends Model
+{
+    protected $table = 'mapel';
+
+    protected $fillable = [
+        'kode','nama','semester'
+    ];
+
+    public function siswa(){
+        return $this->belongsToMany(Siswa::class)->withPivot(['nilai']);
+    }
+}
